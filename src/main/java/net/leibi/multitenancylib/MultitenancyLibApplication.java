2d261cef70c670c04ebe73f4e28848ecc74b6f4f
Andreas Leibrock <git@leibi.net>
2023-11-25 14:12:23 +0100

    initial

---
+++ src/main/java/net/leibi/multitenancylib/MultitenancyLibApplication.java
@@ -0,0 +1,13 @@
+package net.leibi.multitenancylib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitenancyLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultitenancyLibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/leibi/multitenancylib/MultitenancyLibApplicationTests.java
@@ -0,0 +1,13 @@
+package net.leibi.multitenancylib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenancyLibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
